d42639d6bcc10f08df9a007ef3c189d727679e06
Admin <subashbaidya9@gmail.com>
2023-05-31 17:24:33 +0545

    Completed

--- script.ts
+++ script.ts
@@ -1,14 +1,23 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-async function main(){
-    await prisma.user.deleteMany();
+//Ths was just for practice and I have recorded all the codes that were executed ehre somewhere safe ;)
+async function main() {
+
+  const user = await prisma.user.deleteMany({
+    where:{
+      age: {
+        gt: 20
+      }
+    }
+  })
+  console.log(user)
 }
 
 main()
-    .catch(e =>{
-        console.error(e.message);
-    })
-    .finally( async()=>{
-        await prisma.$disconnect;
-    })
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect;
+  });
